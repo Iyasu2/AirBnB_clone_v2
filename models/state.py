0a2739a959aa37b344e799d21b8025d1fc21545a
Ab-Ruh <abell.demere@gmail.com>
2023-09-19 11:57:47 +0300

    sql

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'  # Specify the table name
+    name = Column(String(128), nullable=False)
+
+    # For DBStorage, create a relationship with City
+    cities = relationship(
+        "City",
+        backref="state",
+        cascade="all, delete-orphan")

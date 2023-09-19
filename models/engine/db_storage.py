7ddf3b0dd9ee04e2cd69658ec14a4f786b4858f6
Iyasu2 <126699641+Iyasu2@users.noreply.github.com>
2023-09-19 18:45:23 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,13 @@ from models.base_model import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
-
+from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 class DBStorage:
     """Handles database storage engine"""
@@ -30,13 +36,32 @@ class DBStorage:
 
     def all(self, cls=None):
         """Query all objects of optional class"""
+        classes = {
+            "City": City,
+            "State": State,
+            "User": User,
+            "Place": Place,
+            "Review": Review,
+            "Amenity": Amenity,
+        }
+        result = {}
+        query_rows = []
+        
         if cls:
-            objects = self.__session.query(cls).all()
+            if type(cls) is str:
+                cls = eval(cls)
+            query_rows = self.__session.query(cls)
+            for obj in query_rows:
+                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                result[key] = obj
+            return result
         else:
-            from models.base_model import BaseModel
-            objects = self.__session.query(BaseModel).all()
-
-        return {obj.id: obj for obj in objects}
+            for name, value in classes.items():
+                query_rows = self.__session.query(value)
+                for obj in query_rows:
+                    key = '{}.{}'.format(name, obj.id)
+                    result[key] = obj
+            return result
 
     def new(self, obj):
         """Add object to current session"""

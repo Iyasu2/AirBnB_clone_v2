f3707aa6cadd63315f91ac4cf245a5214f816f21
Ab-Ruh <abell.demere@gmail.com>
2023-09-19 13:57:11 +0300

    ls

--- models/user.py
+++ models/user.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'  # Table name
     email = Column(String(128), nullable=False)  # Email column
     password = Column(String(128), nullable=False)  # Password column
-    first_name = Column(String(128), nullable=True)  # First name column (nullable)
-    last_name = Column(String(128), nullable=True)  # Last name column (nullable)
+    # First name column (nullable)
+    first_name = Column(String(128), nullable=True)
+    # Last name column (nullable)
+    last_name = Column(String(128), nullable=True)

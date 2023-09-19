2c5281f0e88859e5710b80eb4e78db8c89cc6f6b
Ab-Ruh <abell.demere@gmail.com>
2023-09-19 16:11:12 +0300

    return

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-This module initializes the storage system for the AirBnB clone project
-"""
+"""This module instantiates an instance of the Storage will be used"""
 
 from os import getenv
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+storage_type = getenv('HBNB_TYPE_STORAGE')
+
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:

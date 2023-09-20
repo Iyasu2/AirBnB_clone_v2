b94900d02519fe81a0300345fd275945ed8309df
iyasu2 <iyasuforreal@gmail.com>
2023-09-20 21:49:42 +0300

    s

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -15,5 +15,5 @@ class test_Amenity(test_basemodel):
 
     def test_name2(self):
         """ """
-        new = self.value()
-        self.assertNotEqual(type(new.name), str)
+        new = self.value(name='test')
+        self.assertEqual(type(new.name), str)

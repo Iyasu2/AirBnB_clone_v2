7c1bb5bf20e7af02b3afcc3ed01c77e2029f807a
Iyasu2 <iyasuforreal@gmail.com>
2023-10-05 11:43:40 +0300

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo 'Hello, World!' > /data/web_static/releases/test/index.html
+echo 'Hello, World!' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/^server {/a \        location \/hbnb_static\/ {\n                alias \/data\/web_static\/current\/;\n        }' /etc/nginx/sites-available/default
